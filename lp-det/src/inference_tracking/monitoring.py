b1c75abbe1f935351628b1fbd03419efb8221d9a
tabet-anthony <anthony_tabet01@hotmail.com>
2024-12-10 21:32:34 +0200

    Adding the monitoring and the alerting component - Anthony Tabet

--- lp-det/src/inference_tracking/monitoring.py
+++ lp-det/src/inference_tracking/monitoring.py
@@ -3,8 +3,10 @@ from prometheus_client import Counter, Histogram, Gauge, start_http_server
 # A counter to track the number of inferences
 inference_counter = Counter('inferences', 'Number of inference operations')
 
+inference_time = Histogram('inference_time', 'Time spent on inference per frame')
+
 # A histogram to track the processing time of each frame
-processing_time = Histogram('processing_time', 'Time spent processing each frame')
+processed_frames = Counter('processed_frames', 'Total number of processed video frames')
 
 # A gauge to monitor the number of active tracks
 active_tracks = Gauge('active_tracks', 'Number of active tracks at any time')
